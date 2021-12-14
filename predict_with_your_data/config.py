7bb24af1204b69562af5b5084cb07ebf0f393081
gunkaynar <45332095+gunkaynar@users.noreply.github.com>
2021-12-14 23:43:39 +0300

    Update config.py

--- predict_with_your_data/config.py
+++ predict_with_your_data/config.py
@@ -1,10 +1,10 @@
-path = "/mnt/gunkaynar/eretic/pathologic_classification/control_tumor/scripts/pred_quant/toy_example/"
-lib = '/mnt/gunkaynar/refactor_dataset/cpmg/lib/pyNMR'
+path = "/path/to/this/folder/" #please do not forget to put / at the end
+lib = '/path/to/pyNMR/lib' #please do not put / at the end
 
 database = path + "data"
 model_load_base_path = path + "network_per_metabolite"
 
 
 
-shift_ = 0
+shift_ = 0 
 normalization_ = 1
