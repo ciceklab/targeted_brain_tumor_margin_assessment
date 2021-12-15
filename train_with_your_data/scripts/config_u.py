c611150d34d1b634b28b16b21bf2c0f202c49b46
gunkaynar <45332095+gunkaynar@users.noreply.github.com>
2021-12-15 21:16:51 +0300

    Update config_u.py

--- train_with_your_data/scripts/config_u.py
+++ train_with_your_data/scripts/config_u.py
@@ -1,2 +1,3 @@
-base = '/mnt/gunkaynar/git/train_with_your_data'
-lib = "/mnt/gunkaynar/git/train_with_your_data/lib/pyNMR"
+base = '/mnt/gunkaynar/git/train_with_your_data' #please do not put / at the end
+lib = "/mnt/gunkaynar/git/train_with_your_data/lib/pyNMR" #please do not put / at the end
+
